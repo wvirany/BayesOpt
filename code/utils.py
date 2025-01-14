edcffb53e29cadd1ac6d8ab6a4bceb64c1672089
wvirany <waltervirany@gmail.com>
2025-01-14 10:21:51 -0500

    Test log-likelihood evaluation

---
+++ code/utils.py
@@ -0,0 +1,120 @@
+import jax
+import jax.numpy as jnp
+from jax.scipy.stats import multivariate_normal
+import optax
+
+import tanimoto_gp
+
+import datamol as dm
+from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator
+from rdkit.Chem import Crippen
+
+from functools import lru_cache, partial
+
+
+
+def optimize_params(gp, gp_params):
+    """
+    Optimize GP parameters
+    """
+
+    print(f"Start MLL: {gp.marginal_log_likelihood(params=gp_params)}")
+
+    @jax.jit
+    def step(params, opt_state):
+        loss, grads = jax.value_and_grad(lambda x: -gp.marginal_log_likelihood(x))(params)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, loss
+
+    optimizer = optax.adam(1e-2)
+    opt_state = optimizer.init(gp_params)
+
+    # Run optimization loop
+    for _ in range(100):
+        gp_params, opt_state, loss = step(gp_params, opt_state)
+
+    print(f"End MLL (after optimization): {gp.marginal_log_likelihood(params=gp_params)}")
+    print(f"End GP parameters (after optimization): {gp_params}")
+
+    return gp_params
+
+
+
+@lru_cache(maxsize=100_000)
+def smiles_to_fp(smiles: str, fp_type: str = 'ecfp', sparse=True, fpSize=2048):
+    """
+    Convert smiles to sparse count fingerprint of given type
+
+    Arguments:
+        - smiles: SMILES string representing molecule
+        - fp_type: Type of molecular fingerprint
+        - sparse: True for sparse fingerprint, false otherwise
+
+    Accepted fingerprint types:
+        - 'ecfp': Extended connectivity fingerprint
+        - 'fcfp': Functional connectivity fingerprint
+        - 'topological': Topological Torsion Fingerprint
+        - 'atompair': Atom pair fingerprint
+    """
+    mol = Chem.MolFromSmiles(smiles)
+
+    if fp_type == 'ecfp':
+        fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=fpSize)
+    if fp_type == 'fcfp':
+        feature_inv_gen = rdFingerprintGenerator.GetMorganFeatureAtomInvGen()
+        fpgen = rdFingerprintGenerator.GetMorganGenerator(
+            radius=2,
+            atomInvariantsGenerator=feature_inv_gen,
+            fpSize=fpSize
+        )
+    elif fp_type == 'topological':
+        fpgen = rdFingerprintGenerator.GetRDKitFPGenerator(
+            minPath=1,  
+            maxPath=5,
+            useBondOrder=True,  # this replaces bondBits
+            branchedPaths=True,  # this adds branched subgraphs, not just linear paths
+            fpSize=fpSize
+        )
+    elif fp_type == 'atompair':
+        fpgen = rdFingerprintGenerator.GetAtomPairGenerator(
+            minDistance=1,  # minimum number of bonds between atoms
+            maxDistance=5,  # maximum number of bonds between atoms
+            includeChirality=False,  # whether to include chirality in atom invariants
+            use2D=True,  # use 2D (topological) distances rather than 3D
+            fpSize=fpSize
+        )
+    
+    # Returns sparse fingerprint if sparse=True, otherwise returns fingerprint of specified size
+    return fpgen.GetSparseCountFingerprint(mol) if sparse else fpgen.GetCountFingerprint(mol)
+
+
+
+def test_log_likelihood(smiles_test, mean, covar):
+    """
+    Evaluates test log-likelihood given target SMILES strings, mean, and full covariance matrix
+    """
+    
+    y_test_logp = jnp.array([Crippen.MolLogP(Chem.MolFromSmiles(s)) for s in smiles_test])
+    
+    return multivariate_normal.logpdf(y_test_logp, mean=mean, cov=covar)
+
+
+
+def evaluate_gp(smiles_train, y_train, smiles_test, fp_type='ecfp', sparse=True, fpSize=2048):
+    """
+    Evaluate GP with given dataset and fingerprint of given type
+    """
+
+    fp_func = partial(smiles_to_fp, fp_type=fp_type, sparse=sparse, fpSize=fpSize)
+
+    gp = tanimoto_gp.TanimotoGP(fp_func, smiles_train, y_train)
+    gp_params = tanimoto_gp.TanimotoGP_Params(raw_amplitude=jnp.asarray(1.0), raw_noise=jnp.asarray(1e-2))
+    gp_params = optimize_params(gp, gp_params)
+
+    mean, var = gp.predict_y(gp_params, smiles_test, full_covar=True)
+
+    tll = test_log_likelihood(smiles_test, mean, var)
+
+    return mean, var, tll
--- progress.md
+++ progress.md
@@ -1,14 +1,24 @@
-# Jan.
+# Jan 15
+
+
+* Evaluated test log-likelihood for different fingerprints:
+<p align="center">
+ <img src="figures/tll_comparison.png" alt="tll_comparison.png" width="50%"/>
+ </p>
 
 * Tested GP with adjusted mean
   * Computed mean of training data
   * Subtracted mean from training data, yielding "centered" data
   * Trained on centered data
-  * Predicted mean, var of test data, then mean of training data back to predicted mean
+  * Predicted mean, var of test data, then added mean of training data back to predicted mean
 
 $\rightarrow$ Slightly improved results
 
-# Dec. 17
+* Code changes:
+  * Modifications to `tanimoto_gp.py`: included class option for nonzero-mean GP
+
+
+# Dec 17
 
 ### Updates:
 
@@ -24,18 +34,19 @@ $\rightarrow$ Slightly improved results
 
 ### Next steps:
 
-* Currently using ZeroMeanGP $\rightarrow$ Instead, calculate mean of training set and add to mean of GP
-* Visualize test log likelihood
-  * Every data point has a corresponding uncertainty
+- [x] Currently using ZeroMeanGP $\rightarrow$ Instead, calculate mean of training set and add to mean of GP
+
+- [x] Visualize test log likelihood
+  <!-- * Every data point has a corresponding uncertainty
   * Evaluate the log likelihood using [this function](https://jax.readthedocs.io/en/latest/_autosummary/jax.scipy.stats.multivariate_normal.logpdf.html) from JAX
   * Input to M.V. normal is supposed to be y, i.e., give it the mean and var from the GP, then give it y and see the likelihood of that data point
-  * I'll want to get the full covariance matrix for this from the `tanimoto_gp.py` script
-* Make script more general for upcoming Polaris competitions
+  * I'll want to get the full covariance matrix for this from the `tanimoto_gp.py` script -->
+
+- [ ] Make script more general for upcoming Polaris competitions
   * Pass `.csv` files with train/test data as input
   * Script outputs predictions, or submits to Polaris, etc.
   
-
-* Try BO experiments:
+- [ ] Try BO experiments:
   * Use offline dataset (e.g., Polaris logP training dataset)
   * Procedure:
      1. Pick some fraction of dataset (~20%)
@@ -46,4 +57,4 @@ $\rightarrow$ Slightly improved results
      6. Repeat
   * compare to random baseline (i.e., compare to selecting molecule based on uniform distribution)
 
-* Stretch goal: try some experiments w/ less trivial dataset (e.g., DockString, PMO, etc.)
+- [ ] Try some experiments w/ less trivial dataset (e.g., DockString, PMO, etc.)
